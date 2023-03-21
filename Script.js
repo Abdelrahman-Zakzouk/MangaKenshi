b0606054ee19af611f51ed50880f88c7bdec141c
ω〆ƙεɳȿɧɩ ッ <kenshi.wx@gmail.com>
2023-03-21 14:29:28 +0200

    Font Improvement

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ function bodyLoad()
 {
   var typingEffect = new Typed(".author",
   {
-    strings: ["&#120012;&#120013;ƙεɳȿɧɩ ッ","𝔴𝓍𝓀ⅇ𝔫𝔰𝔥𝔦 ッ","WXKENSHI ッ"],
+    strings: ["&#120012;&#120013;&#409;&#x03B5;&#627;&#575;&#615;&#617; &#x30C4;","&#120116;&#119909;&#120000;&#120098;&#120107;&sfr;&hfr;&#120102; &#x30C4;","WXKENSHI &#x30C4;"],
     loop: true,
     typeSpeed: 100,
     backDelay: 1500,
